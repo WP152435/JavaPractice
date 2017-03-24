2c1e2342df12478c532a467bb02a34ae142e3be8
wogml000419 <wogml0123456@naver.com>
2017-03-24 11:46:06 +0900

    자바 실습과제 4-2

--- JavaPractice/src/org/dimigo/oop/Car2.java
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -15,13 +15,25 @@ package org.dimigo.oop;
  * @author : user
  * @version : 1.0
  */
-public class Car {
+public class Car2 {
 	private String company;
 	private String model;
 	private String color;
 	private int maxSpeed;
 	private int price;
 	
+	public Car2()
+	{
+	}
+	
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice)
+	{
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
 	
 	public String getCompany()
 	{
