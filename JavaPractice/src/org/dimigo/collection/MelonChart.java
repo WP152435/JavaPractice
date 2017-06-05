26cdba4d5b9bebd317226299b3e8714182fcee4b
wogml000419 <wogml0123456@naver.com>
2017-06-05 09:39:34 +0900

    실습과제 15

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *   |_ MelonChart
+ * 
+ * 1. 개요: 
+ * 2. 작성일: 2017. 6. 5.
+ * </pre> 
+ *
+ * @author : user
+ * @version : 1.0
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class MelonChart {
+
+	public static void main(String[] args) {
+		
+		List<Music> list = new ArrayList<Music>();
+		list.add(new Music("I LUV IT", "PSY"));
+		list.add(new Music("맞지?", "언니쓰"));
+		
+		System.out.println("-- << 멜론 차트 >> --");
+		printList(list);
+		
+		System.out.println("-- << 2위 곡 추가 >> --");
+		list.add(1, new Music("SIGNAL", "트와이스"));
+		printList(list);
+		
+		System.out.println("-- << 3위 곡 변경 >> --");
+		list.set(2, new Music("팔레트", "아이유"));
+		printList(list);
+		
+		System.out.println("-- << 2위 곡 삭제 >> --");
+		list.remove(1);
+		printList(list);
+		
+		System.out.println("-- << 전체 리스트 삭제 >> --");
+		list.clear();
+		printList(list);
+	}
+	
+	public static void printList(List<Music> list)
+	{
+		int i = 1;
+		for(Music m : list)
+		{
+			System.out.println(i + ". " + m.toString());
+		}
+		System.out.println();
+	}
+
+}
